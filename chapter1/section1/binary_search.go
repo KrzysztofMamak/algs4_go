f0b268ac2b0dedcb42c34b05b4e68fba168761cc
Krzysztof Mamak <krzysztof.mamak2077@gmail.com>
2024-10-30 21:28:06 +0100

    Add BinarySearchRec function

--- chapter1/section1/binary_search.go
+++ chapter1/section1/binary_search.go
@@ -5,8 +5,8 @@ func BinarySearch(key int, a []int) int {
 	hi := len(a) - 1
 
 	for lo <= hi {
-		mid := lo + (hi - lo) / 2
-		
+		mid := lo + (hi-lo)/2
+
 		if key < a[mid] {
 			hi = mid - 1
 		} else if key > a[mid] {
@@ -17,4 +17,24 @@ func BinarySearch(key int, a []int) int {
 	}
 
 	return -1
+}
+
+func BinarySearchRec(key int, a []int) int {
+	return binarySearchRecInner(key, a, 0, len(a))
+}
+
+func binarySearchRecInner(key int, a []int, lo int, hi int) int {
+	if lo <= hi {
+		mid := lo + (hi-lo)/2
+
+		if key < a[mid] {
+			return binarySearchRecInner(key, a, lo, mid-1)
+		} else if key > a[mid] {
+			return binarySearchRecInner(key, a, mid+1, hi)
+		} else {
+			return mid
+		}
+	} else {
+		return -1
+	}
 }
