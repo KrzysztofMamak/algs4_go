4f34995dc86f7d651e6f982149a2546693542c5c
Krzysztof Mamak <krzysztof.mamak2077@gmail.com>
2024-10-31 01:24:50 +0100

    Add ExtendedGcd function

--- chapter1/section1/gcd.go
+++ chapter1/section1/gcd.go
@@ -4,6 +4,17 @@ func Gcd(p int, q int) int {
 	if q == 0 {
 		return p
 	} else {
-		return Gcd(q, p % q)
+		return Gcd(q, p%q)
 	}
+}
+
+// https://cp-algorithms.com/algebra/extended-euclid-algorithm.html
+func ExtendedGcd(a int, b int) (int, int, int) {
+	if b == 0 {
+		return a, 1, 0
+	}
+
+	gcd, x1, y1 := ExtendedGcd(b, a%b)
+	
+	return gcd, y1, x1 - (a/b)*y1
 }
